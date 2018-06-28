de5195d4bc7e19ae8075d55716b63eb3ddbe5b60
KivenLing <KivenL96@163.com>
2018-06-28 18:41:21 +0800

    增加小球类

---
+++ balls/ball.py
@@ -0,0 +1,52 @@
+import abc
+import random
+
+class Ball(object):
+    '''
+    这是游戏内被贪吃蛇吃的球的基类
+    '''
+    def __init__(self, screen, radius):
+        '''
+        初始化基类球 
+        Args:
+            screen: pygame.display.set_mode()对象，表示整个屏幕
+        
+        pos: 是一个包含两个int值的list 例如[1,2]，表示球在屏幕位置，随机生成
+        radius: 是
+        '''
+        self.screen = screen
+        self.pos
+        self.radius = radius
+    
+    @abc.abstractmethod
+    def blitme(self):
+        '''
+        在屏幕中绘制自己图像
+        '''
+        pass
+
+    @abc.abstractmethod
+    def byEat(self):
+        '''
+        被吃之后的效果
+        '''
+        pass
+
+    def isEated(self, snake):
+        '''
+        判断小球是否被蛇吃掉
+        '''
+        # 蛇的位置
+        sx = snake.headX
+        sy = snake.headY
+        # 蛇脑袋的半径
+        sr = snake.r
+
+        # 小球的位置
+        bx = self.pos[0]
+        by = self.pos[1]
+        br = self.radius
+
+        # 返回判断结果
+        # 判断小球与蛇的脑袋是否相撞
+        return (bx-sx)*(bx-sx) + (by-sy)*(by-sy)
---
+++ balls/basicball.py
@@ -0,0 +1,34 @@
+from ball import Ball
+import pygame
+import random
+
+class BasicBall(Ball):
+    '''
+    基本小球，没有特殊属性，被吃后蛇长度以及分数增加1
+    '''
+    def __init__(self, screen):
+        self.screen = screen
+        self.pos = [random.randint(0, 600), random.randint(0, 800)]
+        super(BasicBall, self).__init__(self, screen)
+        # 被吃后增加权重
+        self.weight = 1
+        # 小球半径
+        self.radius = 5
+
+    def blitme(self):
+        '''
+        绘制自己，最基本的小球，圆形，随机颜色，填充
+        '''
+        R = random.randint(50, 150)
+        G = random.randint(50, 150)
+        B = random.randint(50, 150)
+        pygame.draw.circle(self.screen, (R, G, B), \
+        (self.pos[0], self.pos[1]), self.radius, 0)
+
+    def byEat(self):
+        '''
+        小球被吃后，蛇身体增加值以及分数增加值
+        Return:
+               返回分数以及小球身体增加的值
+        '''
+        return self.weight
